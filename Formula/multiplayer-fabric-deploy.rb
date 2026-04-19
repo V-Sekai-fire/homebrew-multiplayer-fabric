7e3a2d80add67d4a4b994d05e57fcb2e75b67917
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-19 14:50:39 -0700

    bump multiplayer-fabric-deploy to v0.4.9, add sccache + scons deps

--- Formula/multiplayer-fabric-deploy.rb
+++ Formula/multiplayer-fabric-deploy.rb
@@ -1,15 +1,17 @@
 class MultiplayerFabricDeploy < Formula
   desc "TUI deploy tool for multiplayer-fabric-godot builds"
   homepage "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy"
-  version "0.4.8"
+  version "0.4.9"
   license "MIT"
 
   depends_on "libgit2"
+  depends_on "sccache"
+  depends_on "scons"
 
   on_macos do
     on_arm do
-      url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/releases/download/v0.4.8/multiplayer_fabric_deploy-v0.4.8-darwin-arm64.tar.gz"
-      sha256 "72999c83b69f29b85fb8a82227b779a3c6402b00f3869dfa4d40ded8cb9539b8"
+      url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/releases/download/v0.4.9/multiplayer_fabric_deploy-v0.4.9-darwin-arm64.tar.gz"
+      sha256 "fbbe3fd5aed1996c5f8f8282cbb2c3b90b5562d43814ab3fafc47df8d5858e26"
     end
   end
 
