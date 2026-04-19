3b529c22bc6ee217247075cda12f583a12b1f61a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-19 13:58:47 -0700

    Bump multiplayer-fabric-deploy to v0.2.0 (shell wrapper fix)

--- Formula/multiplayer-fabric-deploy.rb
+++ Formula/multiplayer-fabric-deploy.rb
@@ -1,9 +1,9 @@
 class MultiplayerFabricDeploy < Formula
   desc "TUI deploy tool for multiplayer-fabric-godot builds"
   homepage "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy"
-  url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "dbdb8b579e4e4f70868c740c3988efe25913363e674b14c614042c30baea10ee"
-  version "0.1.0"
+  url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0e867e33512d45cbc5b2a5b43e0b9c39fa44920f9bf2d85fa57635459bf7efd9"
+  version "0.2.0"
   license "MIT"
 
   depends_on "elixir"
