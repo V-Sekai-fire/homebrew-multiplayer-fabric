9bb5fee202714d096e7d3e6c58ac741e01a0e1ff
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-19 13:16:51 -0700

    feat: add linux arm64/amd64 to formula

--- Formula/multiplayer-fabric-desync.rb
+++ Formula/multiplayer-fabric-desync.rb
@@ -15,6 +15,17 @@ class MultiplayerFabricDesync < Formula
     end
   end
 
+  on_linux do
+    on_arm do
+      url "https://github.com/V-Sekai-fire/multiplayer-fabric-desync/releases/download/v0.0.0/multiplayer-fabric-desync_Linux_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/V-Sekai-fire/multiplayer-fabric-desync/releases/download/v0.0.0/multiplayer-fabric-desync_Linux_amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
   def install
     bin.install "desync"
   end
