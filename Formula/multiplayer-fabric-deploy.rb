54836059244c85c521a91256de376b42944fd53e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-19 14:39:02 -0700

    bump multiplayer-fabric-deploy to v0.4.6

--- Formula/multiplayer-fabric-deploy.rb
+++ Formula/multiplayer-fabric-deploy.rb
@@ -1,15 +1,15 @@
 class MultiplayerFabricDeploy < Formula
   desc "TUI deploy tool for multiplayer-fabric-godot builds"
   homepage "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy"
-  version "0.4.5"
+  version "0.4.6"
   license "MIT"
 
   depends_on "libgit2"
 
   on_macos do
     on_arm do
-      url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/releases/download/v0.4.5/multiplayer_fabric_deploy-v0.4.5-darwin-arm64.tar.gz"
-      sha256 "632bbd82e00ef1e2ed215cd9276619a203b7549101cffbd2840c7634929d7c16"
+      url "https://github.com/V-Sekai-fire/multiplayer-fabric-deploy/releases/download/v0.4.6/multiplayer_fabric_deploy-v0.4.6-darwin-arm64.tar.gz"
+      sha256 "00990edc4cf2c24a34d0ff2ebdcf8e01eddd3f4a100dc9c777bf7f3376eb5170"
     end
   end
 
